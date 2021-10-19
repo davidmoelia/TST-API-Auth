deda5b3521ebbafb9c041894601930956172af68
David M <68513396+davidmoelia@users.noreply.github.com>
2021-10-19 23:36:43 +0700

    new2

--- main.py
+++ main.py
@@ -85,15 +85,10 @@ def get_user(db, username: str):
 def authenticate_user(fake_db, username: str, password: str):
 
     user = get_user(fake_db, username)
-
     if not user:
-
         return False
-
     if not verify_password(password, user.hashed_password):
-
         return False
-
     return user
 
 
