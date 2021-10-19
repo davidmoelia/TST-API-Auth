a684f308a93318b9c947d2a5029eab1b32cb4e38
David M <68513396+davidmoelia@users.noreply.github.com>
2021-10-19 21:23:34 +0700

    ubah secret key

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from pydantic import BaseModel
 
 # to get a string like this run:
 # openssl rand -hex 32
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+SECRET_KEY = "91969153622a3f2c43d7420e19f8811232d5ed0ab46860c3c7a961647c5a6ecf"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
