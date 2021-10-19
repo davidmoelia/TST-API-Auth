916e09ec54db46b12367b216d8259e7d2995b81c
David M <68513396+davidmoelia@users.noreply.github.com>
2021-10-19 23:35:25 +0700

    new

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
+#David Moelia Sapoetra
+#18219012
+#UTS TST API Auth
+
 import json
 from datetime import datetime, timedelta
 from typing import Optional
-
 from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jose import JWTError, jwt
 from passlib.context import CryptContext
 from pydantic import BaseModel
+
+with open("menu.json", "r") as read_file:
+    data = json.load(read_file)
+app = FastAPI()
+
+
 
 # to get a string like this run:
 # openssl rand -hex 32
@@ -15,11 +24,11 @@ ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 
-fake_users_db = {
-    "johndoe": {
-        "username": "johndoe",
-        "full_name": "John Doe",
-        "email": "johndoe@example.com",
+db = {
+    "asdf": {
+        "username": "davidmoelia",
+        "full_name": "David M",
+        "email": "davidmoe@example.com",
         "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
         "disabled": False,
     }
@@ -46,19 +55,24 @@ class UserInDB(User):
     hashed_password: str
 
 
+
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 app = FastAPI()
 
 
+
 def verify_password(plain_password, hashed_password):
+
     return pwd_context.verify(plain_password, hashed_password)
 
+def get_password_hash2(password):
 
-def get_password_hash(password):
     return pwd_context.hash(password)
+
 
 
 def get_user(db, username: str):
@@ -67,13 +81,21 @@ def get_user(db, username: str):
         return UserInDB(**user_dict)
 
 
+
 def authenticate_user(fake_db, username: str, password: str):
+
     user = get_user(fake_db, username)
+
     if not user:
+
         return False
+
     if not verify_password(password, user.hashed_password):
+
         return False
+
     return user
+
 
 
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
@@ -101,7 +123,7 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
         token_data = TokenData(username=username)
     except JWTError:
         raise credentials_exception
-    user = get_user(fake_users_db, username=token_data.username)
+    user = get_user(db, username=token_data.username)
     if user is None:
         raise credentials_exception
     return user
@@ -113,35 +135,20 @@ async def get_current_active_user(current_user: User = Depends(get_current_user)
     return current_user
 
 
-
 @app.post("/token", response_model=Token)
-
 async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
-
-    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
-
+    user = authenticate_user(db, form_data.username, form_data.password)
     if not user:
-
         raise HTTPException(
-
             status_code=status.HTTP_401_UNAUTHORIZED,
-
             detail="Incorrect username or password",
-
             headers={"WWW-Authenticate": "Bearer"},
-
         )
-
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-
     access_token = create_access_token(
-
         data={"sub": user.username}, expires_delta=access_token_expires
-
     )
-
     return {"access_token": access_token, "token_type": "bearer"}
-
 
 
 @app.get("/users/me/", response_model=User)
@@ -154,51 +161,43 @@ async def read_own_items(current_user: User = Depends(get_current_active_user)):
     return [{"item_id": "Foo", "owner": current_user.username}]
 
 
-with open("menu.json", "r") as read_file:
-    data = json.load(read_file)
 
-@app.get('/')
-def root():
-    return{'Menu':'Item'}
+@app.get("/") 
+async def root(token: str = Depends(oauth2_scheme)):
+	return data['menu']
 
 @app.get('/menu/{item_id}')
-async def read_menu(item_id: int):
+async def read_menu(item_id: int, token: str = Depends(oauth2_scheme)):
     for menu_item in data['menu']:
         if menu_item['id'] == item_id:
             return menu_item
     raise HTTPException(
         status_code=404, detail=f'Item not found'
-        )
+    )
 
-@app.post('/menu/{item_id}/{item_name}')
-async def write_menu(name: str):
-    item_id = len(data['menu'])+1
-    newdata = {'id': item_id, 'name' : name}
-    if(item_id > 1):
-        data['menu'].append(newdata)
-        with open("menu.json", "w") as write_file:
-            json.dump(data, write_file)
-        write_file.close()
-        return data
-        
-@app.put('/menu/{item_id}')
-async def update_menu(item_id: int, new_name: str):
-    for menu_item in data['menu']:
-        if menu_item['id'] == item_id:
-            menu_item['name'] = new_name
-        read_file.close()    
-        with open("menu.json", "w") as write_file:
-            json.dump(data, write_file,indent=4)
-        write_file.close()
-    return {'message':'Data changed successfully'}
+@app.post('/menu')
+async def add_menu(name:str, token: str = Depends(oauth2_scheme)):
+	id = len(data['menu']) + 1
+	createdItem = {'id': id, 'name':name}
+	data['menu'].append(createdItem)
+	with open('menu.json', 'w') as write_file:
+		json.dump(data, write_file, indent=4)
+	return data
 
-@app.delete('/menu/{item_id}')
-async def delete_menu(item_id: int):
-    for menu_item in data['menu']:
-        if menu_item['id'] == item_id:
-            data['menu'].remove(menu_item)
-        read_file.close()    
-        with open("menu.json", "w") as write_file:
-            json.dump(data, write_file)
-        write_file.close()
-    return {'message':'Data deleted successfully'}
+@app.delete('/menu/{id}')
+async def delete_menu(id:int, token: str = Depends(oauth2_scheme)):
+	for item in data['menu']:
+		if (item['id'] == id):
+			data['menu'].remove(item)
+	with open('menu.json', 'w') as write_file:
+		json.dump(data, write_file, indent=4)
+	return data
+
+@app.put('/menu/{id}')
+async def update_menu(id:int,name:str, token: str = Depends(oauth2_scheme)):
+	for item in data['menu']:
+		if (item['id'] == id):
+			item['name'] = name
+	with open('menu.json', 'w') as write_file:
+		json.dump(data, write_file, indent=4)
+	return data
